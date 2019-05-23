71dacc39589ce722361c849df848d9f86eb1134e
Tad <tad0616@gmail.com>
2019-05-23 16:44:53 +0800

    編輯並執行商品（Product）的 migrate 檔案

--- database/migrations/2019_05_23_154407_create_products_table.php
+++ database/migrations/2019_05_23_154407_create_products_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateProductsTable extends Migration
 {
@@ -14,7 +14,12 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('image');
+            $table->boolean('on_sale')->default(true);
+            $table->unsignedInteger('price');
             $table->timestamps();
         });
     }
