a33e0a86d25e72d5d5d95c96993ae2c095ca7a41
Tad <tad0616@gmail.com>
2019-05-23 11:44:09 +0800

    新增管理買家的路由

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -5,11 +5,11 @@ use Illuminate\Routing\Router;
 Admin::registerAuthRoutes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
+    'prefix' => config('admin.route.prefix'),
+    'namespace' => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index')->name('admin.home');
-
+    $router->resource('users', UserController::class);
 });
