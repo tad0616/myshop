be187195570b247b1005fd3d18c7c9f04c7f8fac
tad0616 <tad0616@gmail.com>
2019-05-26 11:19:55 +0800

    修改模型以正確顯示圖片

--- app/Product.php
+++ app/Product.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Storage;
 
 class Product extends Model
 {
-    //
+    public function getImageUrlAttribute()
+    {
+        return Storage::disk('public')->url($this->attributes['image']);
+    }
 }
