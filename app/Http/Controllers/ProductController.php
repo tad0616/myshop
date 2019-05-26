199e5a0113bc30d735714daab99792b3dce2b8ec
tad0616 <tad0616@gmail.com>
2019-05-26 16:48:04 +0800

    建立商品詳情頁的控制器

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -47,7 +47,8 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        $product = Product::find($id);
+        return view('product.show', compact('product'));
     }
 
     /**
