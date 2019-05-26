fc87808c0c4486e85098db72883a9cd4ba761e77
tad0616 <tad0616@gmail.com>
2019-05-26 14:48:24 +0800

    讓系統也可以顯示測試圖片

--- app/Product.php
+++ app/Product.php
@@ -3,12 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 use Storage;
 
 class Product extends Model
 {
     public function getImageUrlAttribute()
     {
+        if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
+            return $this->attributes['image'];
+        }
         return Storage::disk('public')->url($this->attributes['image']);
     }
 }
