febfaee8e18bbc21eaef61d90836b9a1622c9a7f
tad0616 <tad0616@gmail.com>
2019-06-02 17:56:05 +0800

    設定批次賦值及關聯

--- app/Order.php
+++ app/Order.php
@@ -2,9 +2,27 @@
 
 namespace App;
 
+use App\OrderItem;
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['user_id', 'address', 'total', 'closed'];
+
+    protected $casts = [
+        'closed' => 'boolean',
+        'address' => 'json',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
 }
--- app/OrderItem.php
+++ app/OrderItem.php
@@ -2,9 +2,21 @@
 
 namespace App;
 
+use App\Order;
+use App\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = ['order_id', 'product_id', 'amount', 'price'];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
