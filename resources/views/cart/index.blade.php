880b97c5ec2de5f26dcd4778b928595563bd7351
tad0616 <tad0616@gmail.com>
2019-06-02 15:05:12 +0800

    調整商品數量

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 @section('content')
+    @php
+        $total = 0;
+    @endphp
+
     <h1>我的購物車</h1>
     <table class="table table-striped">
         <tr>
@@ -22,19 +26,43 @@
                         <div class="warning">该商品已下架</div>
                     @endif
                 </td>
-                <td class="text-right">{{ $cart->product->price }}</td>
-                <td class="text-center">{{ $cart->amount }}</td>
-                <td class="text-right">{{ $cart->product->price * $cart->amount }}</td>
+                <td class="text-right">
+                    <span id="price-{{ $cart->id }}">
+                        {{ $cart->product->price }}
+                    </span>
+                </td>
+                <td class="text-center">
+                    @if($cart->product->on_sale)
+                        <input type="number" min="1" class="form-control text-center amount" name="amount[{{ $cart->product_id }}]" value="{{ $cart->amount }}" data-cartid="{{ $cart->id }}">
+                    @else
+                        <div class="warning">该商品已下架</div>
+                    @endif
+                </td>
+                <td class="text-right">
+                    <span class="sum" id="sum-{{ $cart->id }}">
+                        {{ $cart->product->price * $cart->amount }}
+                    </span>
+                </td>
                 <td nowrap>
                     <a href="#" class="btn btn-danger btn-sm btn-del-from-cart" data-id="{{ $cart->product_id }}">移除</a>
                 </td>
             </tr>
+            @php
+                $total+=$cart->product->price * $cart->amount
+            @endphp
         @empty
 
         <tr>
             <td><h1>購物車空無一物</h1></td>
         </tr>
     @endforelse
+        <tr>
+            <th colspan=4 class="text-right">共計</th>
+            <th nowrap class="text-right">
+                <span id="total">{{ $total }}</span>
+            </th>
+            <th>元</th>
+        </tr>
     </table>
 @endsection
 
@@ -43,6 +71,18 @@
 @section('scriptsAfterJs')
     <script>
         $(document).ready(function () {
+            $('.amount').change(function () {
+                var cartid = $(this).data('cartid');
+                var sum = $(this).val() * $('#price-'+cartid).text();
+                $('#sum-'+cartid).text(sum);
+
+                var total = 0;
+                $('.sum').each(function() {
+                    total += Number($(this).text());
+                });
+                $('#total').text(total);
+            });
+
             $('.btn-del-from-cart').click(function () {
                 var product_id=$(this).data('id');
                 swal({
